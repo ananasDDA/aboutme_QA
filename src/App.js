1cef55a2f6c86b10336316fee99eba0e9a1c5533
daniil <daniil@192.168.1.8>
2025-05-14 21:54:05 +0300

    depl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Experience from './pages/Experience/Experience';
@@ -10,7 +10,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename="/aboutme_QA">
+    <Router>
       <div className="App">
         <Header />
         <main className="main-content">
